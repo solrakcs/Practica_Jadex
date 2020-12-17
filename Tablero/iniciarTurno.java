ecf7ca8dd915a51c38efa730710d713176b0e270
solrakcs <carlitoscs99@gmail.com>
2020-12-17 01:46:00 +0100

    iniciarTurno del Tablero hecho

---
+++ Tablero/iniciarTurno.java
@@ -0,0 +1,45 @@
+import jadex.runtime.IMessageEvent;
+import jadex.runtime.Plan;
+import jadex.runtime.IGoal;
+import jadex.runtime.IBelief;
+import ontology.actions.BeberCerveza;
+import jadex.adapter.fipa.AgentIdentifier;
+import jadex.adapter.fipa.SFipa;
+import jadex.adapter.fipa.AgentDescription;
+import jadex.adapter.fipa.SearchConstraints;
+import jadex.adapter.fipa.ServiceDescription;
+import java.util.Random;
+
+public class IniciarTurno{
+  public body(){
+
+    System.out.println ("Toca cambiar de turno")
+    //Hemos acabado el turno que se estaba realizando
+    //buscamos en el DF al survivor
+    ServiceDescription sd = new ServiceDescription();
+    sd.setType("Registrarse");
+    sd.setName("Survivor")
+    AgentDescription dfadesc = new AgentDescription();
+    dfadesc.addService(sd);
+    SearchConstraints	sc	= new SearchConstraints();
+		sc.setMaxResults(-1); //Mirar lineas 24 y 25
+    IGoal ft = createGoal("df_search");
+    ft.getParameter("description").setValue(dfadesc);
+    ft.getParameter("constraints").setValue(sc);
+    dispatchSubgoalAndWait(ft);
+    System.out.println ("El tablero está buscando al jugador al que le toca actuar");
+    AgentDescription[] result	= (AgentDescription[])ft.getParameterSet("result").getValues();
+    if (result.length>0){
+      System.out.println ("El tablero comunica al jugador que es su turno");
+      	AgentIdentifier survivor = result[0].getName();
+        IMessageEvent msgsend = createMessageEvent("InformarEstadoTablero");
+        ComunicarSituacion accion= new ComunicarSituacion();
+        msgsend.setContent(accion);
+  			msgsend.getParameterSet(SFipa.RECEIVERS).addValue(survivor);
+  			sendMessage(msgsend);
+    } else{
+        	System.out.println("survivor no encontrado");
+
+    }
+  }
+}
